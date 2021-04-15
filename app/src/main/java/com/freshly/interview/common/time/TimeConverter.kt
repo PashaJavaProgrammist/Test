228220408236f222cc893a28865849a886df5b37
Pavel.Haretskiy <pavel.haretskiy@gismart.com>
2021-04-15 09:39:56 +0300

    Add common tests

--- app/src/main/java/com/freshly/interview/common/time/TimeConverter.kt
+++ app/src/main/java/com/freshly/interview/common/time/TimeConverter.kt
@@ -7,6 +7,6 @@ class TimeConverter : DateTimeUtcConverter {
 
     override fun convert(dateTimeUtc: String): String {
         val i = dateTimeUtc.indexOf('T')
-        return dateTimeUtc.substring(i + 1, dateTimeUtc.length - 1)
+        return dateTimeUtc.substring(i + 1, dateTimeUtc.length)
     }
 }
---
+++ app/src/test/java/com/freshly/interview/common/time/TimeConverterTest.kt
@@ -0,0 +1,30 @@
+package com.freshly.interview.common.time
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class TimeConverterTest {
+
+    private val timeConverter = TimeConverter()
+    private val dateConverter = DateConverter()
+
+    @Test
+    fun time_converter_test() {
+        listOf(
+            "10:30:00" to "2021-04-14T10:30:00",
+            "03:30:00" to "2021-04-14T03:30:00"
+        ).map {
+            assertEquals(it.first, timeConverter.convert(it.second))
+        }
+    }
+
+    @Test
+    fun date_converter_test() {
+        listOf(
+            "2021-04-14" to "2021-04-14T10:30:00",
+            "2021-04-14" to "2021-04-14T03:30:00"
+        ).map {
+            assertEquals(it.first, dateConverter.convert(it.second))
+        }
+    }
+}
